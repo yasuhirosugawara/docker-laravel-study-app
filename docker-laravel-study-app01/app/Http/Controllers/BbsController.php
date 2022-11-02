3e3b2c69c2058bce8f2cbec20a481b9a763b74a8
yasuhirosugawara <yasuhiro.sugawara_04@atgp-jobtra.jp>
2022-11-02 11:23:57 +0900

    新規登録時に作成されるレコード内のtimestampカラムがnullのままの事象について、BbsControllerを修正して登録更新日時を作成できるようにしました。

--- docker-laravel-study-app01/app/Http/Controllers/BbsController.php
+++ docker-laravel-study-app01/app/Http/Controllers/BbsController.php
@@ -16,6 +16,8 @@ class BbsController extends Controller
 
     // 投稿された内容を表示するページ
     public function create(Request $request) {
+        $bbs = new Bbs();
+        
 
         // バリデーションチェック
         $request->validate([
@@ -24,10 +26,11 @@ class BbsController extends Controller
         ]);
 
         // 投稿内容の受け取って変数に入れる
-        $name = $request->input('name');
-        $comment = $request->input('comment');
-
-        Bbs::insert(["name" => $name, "comment" => $comment]); // データベーステーブルbbsに投稿内容を入れる
+        // $name = $request->input('name');
+        // $comment = $request->input('comment');
+        $bbs->fill($request->all());
+        $bbs->save();
+        // Bbs::insert(["name" => $name, "comment" => $comment]); // データベーステーブルbbsに投稿内容を入れる
 
         // 変数をビューに渡す
         // return view('bbs.index')->with([
